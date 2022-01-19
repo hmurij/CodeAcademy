8c537097033043e9214c286b76730ee5a025f196
hmurij <dmitrij_jafisov@yahoo.co.uk>
2022-01-19 11:09:07 +0200

    [ND1 - U1] - solution

---
+++ ND1/src/main/java/lt/codeacademy/nd1/Task1.java
@@ -0,0 +1,21 @@
+package lt.codeacademy.nd1;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        print("Vienas");
+
+        String tekstas = "Du";
+        print(tekstas);
+
+    }
+
+    /**
+     * Prints text to console using "standard" output stream
+     *
+     * @param text - String object to print
+     */
+    public static void print(String text) {
+        System.out.println(text);
+    }
+}
