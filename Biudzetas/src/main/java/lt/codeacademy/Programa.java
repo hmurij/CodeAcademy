bdb35fdc22c784fc0f2985fa78d4da50114345c9
hmurij <dmitrij_jafisov@yahoo.co.uk>
2022-01-20 15:01:03 +0200

    [Biudzetas] - Renamed App.java to Programa.java

---
+++ Biudzetas/src/main/java/lt/codeacademy/Programa.java
@@ -0,0 +1,13 @@
+package lt.codeacademy;
+
+/**
+ * Interaktyvia programą, su kurios pagalba vartotojas turi galimybę pasirinkti ką įvesti (pajamas/išlaidas),
+ * turi galimybe gauti reikiamą informaciją kiek išleido ir gavo pajamų.
+ */
+public class Programa
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Interaktyvia programą - Biudzetas" );
+    }
+}
