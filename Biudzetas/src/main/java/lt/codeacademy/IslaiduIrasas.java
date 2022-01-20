37fffa84139bba3a16f3ad5333e4b32c3df21d4b
hmurij <dmitrij_jafisov@yahoo.co.uk>
2022-01-20 17:16:02 +0200

    [Biudzetas] - Created class IslaiduIrasas.

---
+++ Biudzetas/src/main/java/lt/codeacademy/IslaiduIrasas.java
@@ -0,0 +1,77 @@
+package lt.codeacademy;
+
+import java.time.LocalDateTime;
+
+public class IslaiduIrasas {
+    private double suma;
+    private LocalDateTime dataSuLaiku;
+    private IslaiduKategorija kategorija;
+    private AtsiskaitymoBudas atsiskaitymoBudas;
+    private String papildomaInfo;
+
+    public IslaiduIrasas() {
+    }
+
+    public IslaiduIrasas(double suma,
+                         LocalDateTime dataSuLaiku,
+                         IslaiduKategorija kategorija,
+                         AtsiskaitymoBudas atsiskaitymoBudas,
+                         String papildomaInfo) {
+        this.suma = suma;
+        this.dataSuLaiku = dataSuLaiku;
+        this.kategorija = kategorija;
+        this.atsiskaitymoBudas = atsiskaitymoBudas;
+        this.papildomaInfo = papildomaInfo;
+    }
+
+    public double getSuma() {
+        return suma;
+    }
+
+    public void setSuma(double suma) {
+        this.suma = suma;
+    }
+
+    public LocalDateTime getDataSuLaiku() {
+        return dataSuLaiku;
+    }
+
+    public void setDataSuLaiku(LocalDateTime dataSuLaiku) {
+        this.dataSuLaiku = dataSuLaiku;
+    }
+
+    public IslaiduKategorija getKategorija() {
+        return kategorija;
+    }
+
+    public void setKategorija(IslaiduKategorija kategorija) {
+        this.kategorija = kategorija;
+    }
+
+    public AtsiskaitymoBudas getAtsiskaitymoBudas() {
+        return atsiskaitymoBudas;
+    }
+
+    public void setAtsiskaitymoBudas(AtsiskaitymoBudas atsiskaitymoBudas) {
+        this.atsiskaitymoBudas = atsiskaitymoBudas;
+    }
+
+    public String getPapildomaInfo() {
+        return papildomaInfo;
+    }
+
+    public void setPapildomaInfo(String papildomaInfo) {
+        this.papildomaInfo = papildomaInfo;
+    }
+
+    @Override
+    public String toString() {
+        return "IslaiduIrasas{" +
+                "suma=" + suma +
+                ", dataSuLaiku=" + dataSuLaiku +
+                ", kategorija=" + kategorija +
+                ", atsiskaitymoBudas=" + atsiskaitymoBudas +
+                ", papildomaInfo='" + papildomaInfo + '\'' +
+                '}';
+    }
+}
