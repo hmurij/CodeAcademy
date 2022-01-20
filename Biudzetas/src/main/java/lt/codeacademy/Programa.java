6e2cd6c69287de61bf87ed72e8b05f718b058248
hmurij <dmitrij_jafisov@yahoo.co.uk>
2022-01-20 16:47:28 +0200

    [Biudzetas] - Implemented generatePajamuData() and generateComment() methods in main program.

--- Biudzetas/src/main/java/lt/codeacademy/Programa.java
+++ Biudzetas/src/main/java/lt/codeacademy/Programa.java
@@ -1,20 +1,44 @@
 package lt.codeacademy;
 
-import java.util.Arrays;
+import java.time.LocalDate;
+import java.util.Random;
+import java.util.stream.Stream;
 
 /**
  * Interaktyvia programą, su kurios pagalba vartotojas turi galimybę pasirinkti ką įvesti (pajamas/išlaidas),
  * turi galimybe gauti reikiamą informaciją kiek išleido ir gavo pajamų.
  */
-public class Programa
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Interaktyvia programą - Biudzetas" );
+public class Programa {
+    public static void main(String[] args) {
+        System.out.println("Interaktyvia programą - Biudzetas");
 
-        Arrays.stream(PajamuKategorija.values()).forEach(k -> {
-            System.out.println(k);
-            System.out.println(k.getKaterorija());
-        });
+        Stream.generate(Programa::generatePajamuData).limit(10).forEach(System.out::println);
+    }
+
+    /**
+     * Generates random PajamuIrasas object
+     * @return random PajamuIrasas object
+     */
+    public static PajamuIrasas generatePajamuData() {
+        Random r = new Random();
+        PajamuKategorija[] kategorijas = PajamuKategorija.values();
+
+        return new PajamuIrasas(r.nextDouble() * 1000,
+                LocalDate.now().minusDays(r.nextInt(100)),
+                kategorijas[r.nextInt(kategorijas.length)],
+                r.nextBoolean(),
+                generateComment());
+    }
+
+    /**
+     * Generates random dummy text comment
+     * @return random dummy text comment
+     */
+    public static String generateComment() {
+        String[] comments = {"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa." +
+                "Lorem ipsum dolor sit amet.", "Lorem ipsum dolor.", "Lorem ipsum dolor sit amet, consectetuer adipiscing",
+                "Lorem ipsum"};
+
+        return comments[new Random().nextInt(comments.length)];
     }
 }
