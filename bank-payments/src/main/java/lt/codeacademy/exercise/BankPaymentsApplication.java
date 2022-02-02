600a6f4f18b0ab7ffc2dd351a5925230f52ddd5b
Dmitrij Jafisov <dmitrij_jafisov@yahoo.co.uk>
2022-02-02 14:07:51 +0200

    [bank-payments] - implemented readSvedData method in BankPaymentsApplication. Updated class Record.

--- bank-payments/src/main/java/lt/codeacademy/exercise/BankPaymentsApplication.java
+++ bank-payments/src/main/java/lt/codeacademy/exercise/BankPaymentsApplication.java
@@ -3,6 +3,7 @@ package lt.codeacademy.exercise;
 import lt.codeacademy.ConsolePrinter;
 import lt.codeacademy.exercise.bank.data.LumiData;
 import lt.codeacademy.exercise.bank.data.SepData;
+import lt.codeacademy.exercise.bank.data.ShvedData;
 import lt.codeacademy.exercise.menu.console.ConsoleMenu;
 import lt.codeacademy.exercise.menu.console.MainMenu;
 
@@ -42,13 +43,14 @@ public class BankPaymentsApplication {
         switch (selectedOption) {
             case "1":
                 readLumiData(LumiData.MOCK_DATA);
-//                ConsolePrinter.print("Reading formatted \"LumiData\" payments");
+                ConsolePrinter.print("Reading formatted \"LumiData\" payments");
                 break;
             case "2":
                 readSepData(SepData.MOCK_DATA);
-//                ConsolePrinter.print("Reading formatted \"SepData\" payments");
+                ConsolePrinter.print("Reading formatted \"SepData\" payments");
                 break;
             case "3":
+                readSvedData(ShvedData.MOCK_DATA);
                 ConsolePrinter.print("Reading formatted \"ShvedData\" payments");
                 break;
             case "4":
@@ -59,40 +61,83 @@ public class BankPaymentsApplication {
 
     /**
      * Reads Lumi bank data and populates bank records
+     * @param data Lumi bank data
      */
     private void readLumiData(String [][] data){
         // Debtor name, Payment date, IBAN, Amount
         // "Karlson und Meier KG", "2022-0-14", "DE16333333330000002222", "10.12"
 
         for(int i = 1; i < data.length; i++){
-            records.add(new Record(LocalDate.parse(data[i][1]),
+            records.add(new Record("Lumi",
+                    LocalDate.parse(data[i][1]),
                     Double.parseDouble(data[i][3]),
                     data[i][0],
                     data[i][2]));
         }
 
-//        records.forEach(System.out::println);
     }
 
+    /**
+     * Reads Seb bank data and populates bank records
+     * @param data Sep bank data
+     */
     private void readSepData(String [][] data){
         // "Operation Date", "Payer Name", "IBAN", "Amount"
         // "04.01.2022", "Simpson S", "SP60123456781234567891", "431.00"
 
-//        private LocalDate date;
-//        private double amount;
-//        private String payerName;
-//        private String accountNr;
-
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
         for(int i = 1; i < data.length; i++){
-            records.add(new Record(LocalDate.parse(data[i][0], formatter),
+            records.add(new Record("Seb",
+                    LocalDate.parse(data[i][0], formatter),
                     Double.parseDouble(data[i][3]),
                     data[i][1],
                     data[i][2]));
         }
 
-//        records.forEach(System.out::println);
+    }
 
+    /**
+     * Reads Shved bank data and populates bank records
+     * @param data Sep bank data
+     */
+    private void readSvedData(String[][] data) {
+        // "Execution date", "IBAN", "Paid Amount"
+        // "02.01.2022", "LT16333333330000002222", "10,12"
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+        for(int i = 1; i < data.length; i++){
+            records.add(new Record("Shved",
+                    LocalDate.parse(data[i][0], formatter),
+                    Double.parseDouble(data[i][2].replace(",", ".")),
+                    "",
+                    data[i][1]));
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- bank-payments/src/main/java/lt/codeacademy/exercise/Record.java
+++ bank-payments/src/main/java/lt/codeacademy/exercise/Record.java
@@ -4,16 +4,26 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 public class Record {
+    private String bankName;
     private LocalDate date;
     private double amount;
     private String payerName;
     private String accountNr;
 
-    public Record(LocalDate date, double amount, String payerName, String accountNr) {
+    public Record(String bankName, LocalDate date, double amount, String payerName, String accountNr) {
+        this.bankName = bankName;
         this.date = date;
         this.amount = amount;
         this.payerName = payerName;
         this.accountNr = accountNr;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
     }
 
     public LocalDate getDate() {
@@ -53,19 +63,23 @@ public class Record {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Record record = (Record) o;
-        return Double.compare(record.amount, amount) == 0 && Objects.equals(date, record.date)
-                && Objects.equals(payerName, record.payerName) && Objects.equals(accountNr, record.accountNr);
+        return Double.compare(record.amount, amount) == 0 &&
+                Objects.equals(bankName, record.bankName) &&
+                Objects.equals(date, record.date) &&
+                Objects.equals(payerName, record.payerName) &&
+                Objects.equals(accountNr, record.accountNr);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(date, amount, payerName, accountNr);
+        return Objects.hash(bankName, date, amount, payerName, accountNr);
     }
 
     @Override
     public String toString() {
         return "Record{" +
-                "date=" + date +
+                "bankName='" + bankName + '\'' +
+                ", date=" + date +
                 ", amount=" + amount +
                 ", payerName='" + payerName + '\'' +
                 ", accountNr='" + accountNr + '\'' +
