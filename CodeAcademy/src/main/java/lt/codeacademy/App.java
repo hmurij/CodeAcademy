69e8d6cf04b8f218c61c1581d0c6d241d860d8b1
Dmitrij Jafisov <dmitrij_jafisov@yahoo.co.uk>
2022-02-02 19:30:52 +0200

    [lesson14/String_format] - solution to Task 1&2

--- CodeAcademy/src/main/java/lt/codeacademy/App.java
+++ CodeAcademy/src/main/java/lt/codeacademy/App.java
@@ -1,13 +1,40 @@
 package lt.codeacademy;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.util.Arrays;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.printf("|%20s|\n|%20s|\n|%20s|\n|%20s|\n|%-20s|\n", "As", "tikrai", "ismoksiu", "programuoti", "Java");
+
+        String[] sa = new String[5];
+        sa[0] = "one";
+        sa[1] = "two";
+
+        System.out.println();
+        System.out.println(putInArray(sa, "three"));
+        System.out.println(Arrays.toString(sa));
+        System.out.println(putInArray(sa, "four"));
+        System.out.println(Arrays.toString(sa));
+        System.out.println(putInArray(sa, "five"));
+        System.out.println(Arrays.toString(sa));
+        System.out.println(putInArray(sa, "six"));
+        System.out.println(Arrays.toString(sa));
+
+    }
+
+    public static boolean putInArray(String[] array, String text) {
+
+        boolean result = false;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] == null){
+                array[i] = text;
+                result = true;
+                break;
+            }
+        }
+
+        return result;
     }
 }
