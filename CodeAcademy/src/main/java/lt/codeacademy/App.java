6e2fad7c38449f7913d9b5be09dafc05cc963fd2
Dmitrij Jafisov <dmitrij_jafisov@yahoo.co.uk>
2022-02-19 12:31:16 +0200

    [lesson22/generics] - wrong commit.

--- CodeAcademy/src/main/java/lt/codeacademy/App.java
+++ CodeAcademy/src/main/java/lt/codeacademy/App.java
@@ -1,18 +1,7 @@
 package lt.codeacademy;
 
-import lt.codeacademy.model.Person;
-
 public class App {
     public static void main(String[] args) {
-        GenericBox<Person> personListBox = new PersonListBox();
 
-        printArray(new Integer[]{1, 2, 3});
-        printArray(new String[]{"Hello", "World"});
-    }
-
-    private static <T> void printArray(T[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-        }
     }
 }
--- CodeAcademy/src/main/java/lt/codeacademy/PersonListBox.java
+++
@@ -1,7 +0,0 @@
-package lt.codeacademy;
-
-import lt.codeacademy.model.Person;
-
-public class PersonListBox extends GenericBox<Person> {
-
-}
