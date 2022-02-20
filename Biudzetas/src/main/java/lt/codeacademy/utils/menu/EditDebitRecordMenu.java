5edb82adfb92306aca916f08b846655d31c68e17
Dmitrij Jafisov <dmitrij_jafisov@yahoo.co.uk>
2022-02-20 22:33:05 +0200

    [Budzetas 2 dalis] - created class EditDebitRecordMenu.

---
+++ Biudzetas/src/main/java/lt/codeacademy/utils/menu/EditDebitRecordMenu.java
@@ -0,0 +1,10 @@
+package lt.codeacademy.utils.menu;
+
+import lt.codeacademy.budget.Budget;
+
+public class EditDebitRecordMenu {
+
+    public void editDebitRecordMenu(Budget budget){
+        System.out.println("Iveskyte redaguojamo ");
+    }
+}
