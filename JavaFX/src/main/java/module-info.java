22853e2bffcfc53dc1479aa1fcacd4ccb5872797
Dmitrij Jafisov <dmitrij_jafisov@yahoo.co.uk>
2022-02-28 21:54:53 +0200

    [lesson27/JavaFX] - new project

---
+++ JavaFX/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module lt.codeacademy.javafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens lt.codeacademy.javafx to javafx.fxml;
+    exports lt.codeacademy.javafx;
+}
