58db268101a59e5e751f8b25cacddc22fc1b7ba0
Dmitrij Jafisov <dmitrij_jafisov@yahoo.co.uk>
2022-05-12 02:24:18 +0300

    [springMVC] - testing git

--- Spring/01_spring_mvc-thymeleaf/src/main/java/com/codeademy/lt/NotFoundException.java
+++ Spring/01_spring_mvc-thymeleaf/src/main/java/com/codeademy/lt/NotFoundException.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 //@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
     private final UUID productId;
+    private int myInt;
+    private String str;
 
     public NotFoundException(UUID productId) {
         super();
